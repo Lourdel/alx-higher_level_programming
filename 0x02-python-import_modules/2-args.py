b309f785dfe71aa0bb128cdab45c472a7a2e3eec
Lourdel <klourdel@gmail.com>
2022-09-22 16:12:07 +0000

    fixing the pycodestyle error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    i = len(argv) - 1 
+    i = len(argv) - 1
     if i == 0:
         print("{} arguments.".format(i))
     elif i == 1:
