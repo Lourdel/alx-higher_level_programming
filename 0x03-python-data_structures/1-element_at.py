4890b80db13249e1f2702597389f1f2aa209664e
Lourdel <klourdel@gmail.com>
2022-09-23 17:07:03 +0000

    retrieves an element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for element in my_list:
+        if idx < 0 and idx >= len(my_list):
+            return (None)
+        else:
+            element = my_list[idx]
+            return (element)
