7d1966db56cd8df073f7dffe94d7a2c3d3ffe587
Lourdel <klourdel@gmail.com>
2022-10-24 18:59:24 +0000

    fixing errors

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"A module defining a class MyList"
+
 
 class MyList(list):
     """class that inherits attributes from list
 
     Args:
         list: class list
+
+
     """
 
     def print_sorted(self):
