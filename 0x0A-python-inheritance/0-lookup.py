79c405af1618e2f95734c113e9ec149f42ab4e23
Lourdel <klourdel@gmail.com>
2022-10-24 19:05:46 +0000

    Fixing errors

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+Module for the lookup method
+"""
+
 
 def lookup(obj):
-    """lookup returns a list of available attributes"""
+    """lookup returns a list of available attributes
+
+    Args:
+        obj: instance of a class
+
+    Returns:
+        List of attributes
+    """
+
     return (dir(obj))
