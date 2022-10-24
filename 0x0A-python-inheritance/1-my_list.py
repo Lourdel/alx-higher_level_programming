0c65a8bd133dc0561a192164b4c4cb961ff78b39
Lourdel <klourdel@gmail.com>
2022-10-24 18:55:39 +0000

    fixing errors

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 class MyList(list):
-   """class that inherits attributes from list
-    
+    """class that inherits attributes from list
+
     Args:
         list: class list
-   """ 
-    
+    """
+
     def print_sorted(self):
         """method that prints a sorted list"""
         print(sorted(self))
